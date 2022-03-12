4304ff06adc886f986ca70a8fd9a150cde0963fe
ravinduf <ravinduf@rocketmail.com>
2022-03-12 13:20:29 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = conferenceTickets
+
+	// fmt.Println("Welcome to", conferenceName, "booking appllication")
+	// fmt.Println("Get your tickets here to attend")
+	// fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	
+	
+	fmt.Println(conferenceTickets)
+}
