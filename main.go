48e420293dec05d3dde503d242dce6262de1bca7
ravinduf <ravinduf@rocketmail.com>
2022-03-18 10:57:14 +0530

    added bookings

--- main.go
+++ main.go
@@ -18,12 +18,16 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	// var bookings = [50]string{"Ravindu", "Senal"}
+	var bookings [50]string
+	
+	
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
 	// ask user for their name
-
+	
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
 	
@@ -36,10 +40,17 @@ func main() {
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 	// fmt.Scan(&userTickets)
-
+	
 	// userName = "Tom"
-
+	
 	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\v", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. \nYou will recieve a confirmation email at %v\n", 
 		firstName, lastName, userTickets, email)
